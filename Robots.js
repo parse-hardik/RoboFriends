bc509ca5a45f58c4f5971605536cf5c1efe67516
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2019-12-08 02:20:34 +0530

    Correcting the web display

---
+++ Robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    color: 'orange'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    color: 'light-green'
+  },
+  {
+    id: 3,
+    name: 'Oompie',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    color: 'red'
+  },
+  {
+    id: 4,
+    name: 'Negi Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+    color: 'silver'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+    color: 'silver'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info',
+    color: 'pink'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+    color: 'dark-blue'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me',
+    color: 'light-green'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io',
+    color: 'washed-red'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz',
+    color: 'gold'
+  }
+];
